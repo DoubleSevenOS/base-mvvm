2c6399dc65e05ca1b7aa3e33f8dfe2ebcb84b683
huyangyang <huyangyang0801@gmail.com>
2021-06-16 16:52:05 +0800

    更新bug

--- Base/src/main/java/com/android/base/base/CommonBindAdapter.java
+++ Base/src/main/java/com/android/base/base/CommonBindAdapter.java
@@ -3,10 +3,4 @@ package com.android.base.base;
 import androidx.databinding.BindingAdapter;
 
 public class CommonBindAdapter {
-
-    @BindingAdapter("recycleData")
-    public static void setRecycleViewData() {
-
-    }
-
 }
